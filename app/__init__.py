5f92264505718840cb12728bd58eacb2c036c153
Eduv7 <85246857+eduardov7@users.noreply.github.com>
2024-08-18 23:20:20 -0400

    d

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from app.db import create_table  # Importa la función para crear la tabla
 
 app = Flask(__name__)
+
+# Crear la tabla cuando se inicializa la aplicación
+create_table()
 
 # Asegúrate de importar las rutas aquí, después de crear la instancia de la aplicación
 from app import routes
--- app/db.py
+++ app/db.py
@@ -4,3 +4,16 @@ from config import Config
 def get_db_connection():
     conn = psycopg2.connect(Config.DATABASE_URL)
     return conn
+
+def create_table():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS items (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL
+        );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
