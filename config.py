d06b748a3dc9e8fcb383e8284e15549713049da9
Eduv7 <85246857+eduardov7@users.noreply.github.com>
2024-08-18 23:04:53 -0400

    u

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    DATABASE_URL = 'postgresql://edu:Qfv3sz5jkQltpluUDMCXPahdQFa1UPd@dpg-cr1b4qjtq21c73cr0ke0-a.oregon-postgres.render.com:5432/crud_7rkx'
+    DATABASE_URL = 'postgresql://edu:Qfv3sz5jkQltpluUDMCXPahdQFa1UPd@dpg-cr1b4qjtq21c73cr0ke0-a.oregon-postgres.render.com:5432/crud_7rkx?sslmode=require'
 
