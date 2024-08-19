a321d433038018b915b628eeefaa967202b96f1f
Eduv7 <85246857+eduardov7@users.noreply.github.com>
2024-08-18 22:57:13 -0400

    ad

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
 class Config:
-    DATABASE_URL = 'postgresql://postgres:1213@localhost:7777/crud'
-
+    DATABASE_URL = 'postgresql://edu:Qfv3sz5jkQltpluUDMCXPahdQFa1UPd@dpg-cr1b4qjtq21c73cr0ke0-a:5432/crud_7rkx'
